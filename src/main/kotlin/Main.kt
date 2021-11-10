70b27b5b7cd01bff800462b58ad61aafddaff57f
Joel Handwell <joelhandwell@gmail.com>
2021-11-10 11:13:50 +0900

    Upto 2 ^ 1000

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,7 +9,7 @@ fun humanReadableByteCount(bytes: BigDecimal, si: Boolean = true): String? {
         return "$bytes B"
     var result = bytes
     val unitsToUse =
-        (if (si) "k" else "K") + "MGTPEZYRQ" // ronna quecca from https://www.science.org/content/article/you-know-kilo-mega-and-giga-metric-system-ready-ronna-and-quecca
+        (if (si) "k" else "K") + "MGTPEZYRQXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX" // ronna quecca from https://www.science.org/content/article/you-know-kilo-mega-and-giga-metric-system-ready-ronna-and-quecca
     var i = 0
     val unitsCount = unitsToUse.length
     while (true) {
@@ -28,7 +28,7 @@ fun humanReadableByteCount(bytes: BigDecimal, si: Boolean = true): String? {
 }
 
 fun main() {
-    (1..100).forEach {
+    (1..1000).forEach {
         val power = it
         val number = BigDecimal.valueOf(2).pow(power)
         val withPrefix = humanReadableByteCount(number)
